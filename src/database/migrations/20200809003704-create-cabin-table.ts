494460802f9965909ba90ba7a71d59e31e4059b3
Aléxia <lecavsalexia@gmail.com>
2020-08-08 21:41:15 -0300

    feat(migrations): add cabin table creation.

---
+++ src/database/migrations/20200809003704-create-cabin-table.ts
@@ -0,0 +1,31 @@
+import SequelizeStatic, { QueryInterface } from 'sequelize'
+import { CabinModel } from '../../models/CabinModel'
+
+export = {
+	up: (queryInterface: QueryInterface, Sequelize: any) => {
+		return queryInterface.createTable<CabinModel>('cabins', {
+			idCabin: {
+				primaryKey: true,
+				allowNull: false,
+				autoIncrement: true,
+				type: SequelizeStatic.BIGINT,
+			},
+			dsImageURL: {
+				allowNull: false,
+				type: SequelizeStatic.INTEGER,
+			},
+			dsName: {
+				allowNull: false,
+				type: SequelizeStatic.STRING,
+			},
+			tpDivinityRelated: {
+				allowNull: false,
+				type: SequelizeStatic.STRING,
+			},
+		})
+	},
+
+	down: (queryInterface: QueryInterface, Sequelize: any) => {
+		return queryInterface.dropTable('cabin')
+	},
+}
