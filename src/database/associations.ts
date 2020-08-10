fe0a8ca26babcc3a8083b05c0f46be8902c44221
Aléxia <lecavsalexia@gmail.com>
2020-08-10 20:16:24 -0300

    feat(associations): create associations for edition and cabin request.

--- src/database/associations.ts
+++ src/database/associations.ts
@@ -1,15 +1,45 @@
-import { Includeable } from 'sequelize/types'
-import { Cabin, Camper } from '../models'
+import { Includeable, Model, ModelStatic } from 'sequelize/types'
+import { Cabin, Camper, CabinRequest, CabinAttributes, CabinCreationAttributes, Edition } from '../models'
 
-Camper.associate = function() {
-	this.belongsTo(Cabin, { foreignKey: 'idCabin', as: 'cabin' })
+Camper.associate = function(): void {
+	Camper.belongsTo(Cabin, { foreignKey: 'idCabin', as: 'cabin' })
+	Camper.hasMany(CabinRequest, { foreignKey: 'idCamper', as: 'camper' })
 }
 
-Cabin.associate = function() {
-	this.hasMany(Camper, { foreignKey: 'idCabin', as: 'campers' })
+Cabin.associate = function(): void {
+	Cabin.hasMany(Camper, { foreignKey: 'idCabin', as: 'campers' })
+	Cabin.belongsToMany(CabinRequest, {
+		foreignKey: 'idFirstOptionCabin',
+		as: 'firstOptionCabin',
+		through: CabinRequest.tableName,
+	})
+	Cabin.belongsToMany(CabinRequest, {
+		foreignKey: 'idSecondOptionCabin',
+		as: 'secondOptionCabin',
+		through: CabinRequest.tableName,
+	})
+	Cabin.belongsToMany(CabinRequest, {
+		foreignKey: 'idThirdOptionCabin',
+		as: 'thirdOptionCabin',
+		through: CabinRequest.tableName,
+	})
+}
+
+Edition.associate = function(): void {
+	Edition.hasMany(CabinRequest)
+}
+
+CabinRequest.associate = function(): void {
+	CabinRequest.hasOne(Edition, { foreignKey: 'idEdition' })
+	CabinRequest.hasOne(Cabin, { foreignKey: 'idFirstOptionCabin' })
+	CabinRequest.hasOne(Cabin, { foreignKey: 'idSecondOptionCabin' })
+	CabinRequest.hasOne(Cabin, { foreignKey: 'idThirdOptionCabin' })
+	CabinRequest.hasOne(Camper, { foreignKey: 'idCamper' })
 }
 
 Camper.associate()
 Cabin.associate()
+Edition.associate()
+CabinRequest.associate()
 
 export const INCLUDE_CAMPER = { model: Camper, as: 'campers' } as Includeable
