74a3c42c6270b9d8276db76bb87e3c56a7f69a9e
Aléxia <lecavsalexia@gmail.com>
2020-08-11 00:50:45 -0300

    feat(cabinRequest): add check function to controller.

--- src/api/controllers/CabinRequestController.ts
+++ src/api/controllers/CabinRequestController.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from 'express'
-import { CabinRequestAttributes, CabinRequest } from '../../models'
+import { CabinRequestAttributes, CabinRequest, Status } from '../../models'
+import { EditionService } from '../services/EditionService'
 
 export class CabinRequestController {
+	constructor(private editionService: EditionService) {
+		this.create = this.create.bind(this)
+		this.camperHasRequestedCabin = this.camperHasRequestedCabin.bind(this)
+	}
+
 	public async create(req: Request, res: Response<CabinRequestAttributes>): Promise<void> {
 		const body = req.body as CabinRequestAttributes
 		const created = await CabinRequest.create(body)
 		res.json(created)
+	}
+
+	public async camperHasRequestedCabin(req: Request, res: Response): Promise<void> {
+		const { idCamper } = req.params
+		const currentEdition = await this.editionService.findCurrent()
+		const number = await CabinRequest.count({
+			where: {
+				idCamper,
+				idEdition: currentEdition.idEdition,
+				status: Status.UNRESOLVED,
+			},
+		})
+
+		console.log(number)
+
+		res.json(Boolean(number))
 	}
 }
