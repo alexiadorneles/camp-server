25c1c97dc234a37bb87d12b456c330071575f341
alexiadorneles <lecavsalexia@gmail.com>
2020-08-22 22:54:06 -0300

    feat(type): add IndexedObject type.

--- src/types/Data.d.ts
+++ src/types/Data.d.ts
@@ -1,4 +1,8 @@
-export interface TimestampDependant {
+export type IndexedObject = {
+	[key in string]: any
+}
+
+export interface TimestampDependant extends IndexedObject {
 	createdAt: Date
 	updatedAt: Date
 }
