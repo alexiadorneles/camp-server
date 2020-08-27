d50f39e88320f77b946d7f26e000eedf347fdbb9
alexiadorneles <lecavsalexia@gmail.com>
2020-08-26 22:34:43 -0300

    feat(auth): type JWT Payload.

--- src/api/routes/JWTMediator.ts
+++ src/api/routes/JWTMediator.ts
@@ -1,15 +1,19 @@
 import jwt from 'jsonwebtoken'
 
 const SECRET = 'bG9vayBpIGRpZG4ndCB3YW50IHRvIGJlIGEgaGFsZiBibG9vZA=='
+export interface JWTPayload {
+	idCamper: number
+	password: string
+}
 
 export class JWTMediator {
 	private constructor() {}
 
-	public static sign(payload: any): string {
+	public static sign(payload: JWTPayload): string {
 		return jwt.sign(payload, SECRET, { expiresIn: '24h' })
 	}
 
-	public static verify(token: string): string | object {
-		return jwt.verify(token, SECRET)
+	public static verify(token: string): JWTPayload {
+		return jwt.verify(token, SECRET) as JWTPayload
 	}
 }
