bdd2737029d8a0bd31d80fb9835f22d37201cc30
alexiadorneles <lecavsalexia@gmail.com>
2020-08-27 21:38:37 -0300

    fix(middleware): compare id when is present.

--- src/api/routes/OwnerMiddleware.ts
+++ src/api/routes/OwnerMiddleware.ts
@@ -3,7 +3,9 @@ import { Request, Response } from 'express'
 export async function ownerMiddleware(req: Request, res: Response, next: Function): Promise<void> {
 	const idCamper = Number(req.params.signedInUser)
 	const paramId = Number(req.params.idCamper)
-	if (paramId !== idCamper) {
+	const bodyId = req.body && req.body.idCamper
+	const id = paramId || bodyId
+	if (id && id !== idCamper) {
 		res.status(403).json({ error: "You don't have permission to perform this action" })
 		return
 	}
