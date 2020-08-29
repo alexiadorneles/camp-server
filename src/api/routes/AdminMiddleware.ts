522c679eb5ba3fa16519ffc8380bc1857b4ee574
alexiadorneles <lecavsalexia@gmail.com>
2020-08-28 21:21:48 -0300

    feat(admin): create admin middleware.

---
+++ src/api/routes/AdminMiddleware.ts
@@ -0,0 +1,33 @@
+import bcrypt from 'bcryptjs'
+import { Request, Response } from 'express'
+import { JWTMediator } from './JWTMediator'
+import { Admin } from '../../models/AdminModel'
+
+export async function adminMiddleware(req: Request, res: Response, next: Function): Promise<void> {
+	const [, token] = req.headers.authorization.split(' ')
+	try {
+		const { idGoogleAdmin, password } = await JWTMediator.verify(token)
+		if (!idGoogleAdmin || !password) {
+			res.status(403).json('Nice try')
+			return
+		}
+
+		const adm = await Admin.findOne({ where: { idGoogle: idGoogleAdmin } })
+		if (!adm) {
+			res.status(403).json(`You're not an Admin`)
+			return
+		}
+
+		const correctPassword = await bcrypt.compare(password, adm.password)
+		if (!correctPassword) {
+			res.status(403).json(`Incorrect Password`)
+			return
+		}
+
+		next()
+	} catch (error) {
+		res.status(403).json({ error })
+	}
+}
+
+// 116076790618527276238
