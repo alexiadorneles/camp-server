12bd4bde28ac50f62972bf1e1330e05cb9777760
alexiadorneles <lecavsalexia@gmail.com>
2020-08-30 18:19:36 -0300

    feat(db): add db.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const { DATABASE_URL, DATABASE_NAME, DATABASE_USER_NAME, DATABASE_PASSWORD } = process.env
+
 module.exports = {
 	dialect: 'mysql',
-	host: 'localhost',
-	username: 'camp',
-	password: 'camphalfbloodPPJ2020',
-	database: 'camp',
+	host: DATABASE_URL,
+	username: DATABASE_USER_NAME,
+	password: DATABASE_PASSWORD,
+	database: DATABASE_NAME,
 	define: {
 		timestamps: true,
 	},
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const { DATABASE_URL, DATABASE_NAME, DATABASE_USER_NAME, DATABASE_PASSWORD } = process.env
+
 export default {
 	dialect: 'mysql',
-	host: 'localhost',
-	username: 'camp',
-	password: 'camphalfbloodPPJ2020',
-	database: 'camp',
+	host: DATABASE_URL,
+	username: DATABASE_USER_NAME,
+	password: DATABASE_PASSWORD,
+	database: DATABASE_NAME,
 	define: {
 		timestamps: true,
 	},
