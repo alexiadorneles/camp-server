0c9c9182651c49b5ce8cfb480488a5f5d3bdd28f
alexiadorneles <lecavsalexia@gmail.com>
2021-01-06 23:22:36 -0300

    refactor(activityRound): improve logic.

--- src/api/controllers/RoundController.ts
+++ src/api/controllers/RoundController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import _ from 'lodash'
 import { Includeable, Op } from 'sequelize'
-import { Camper, CamperEdition, Round, Activity, ActivityOption, CamperActivity } from '../../models'
+import { Activity, ActivityOption, CamperActivity, CamperEdition, Round } from '../../models'
 import { RoundConfig } from '../../types/RoundConfig'
 import { EditionService } from '../services'
 import { ActivityService } from '../services/ActivityService'
@@ -9,6 +9,7 @@ import { ActivityService } from '../services/ActivityService'
 export class RoundController {
 	constructor(private editionService: EditionService, private activityService: ActivityService) {
 		this.generateRoundFromConfig = this.generateRoundFromConfig.bind(this)
+		this.loadRoundForCamper = this.loadRoundForCamper.bind(this)
 	}
 
 	public async generateRoundFromConfig(req: Request, res: Response): Promise<void> {
@@ -16,37 +17,18 @@ export class RoundController {
 		const { idEdition } = await this.editionService.findCurrent()
 		const allCampersFromThisEdition = await CamperEdition.findAll({
 			where: { idEdition },
-			include: { model: Camper, as: 'camper' } as Includeable,
+			attributes: ['idCamper'],
 		})
 
 		const idsCampers = allCampersFromThisEdition.map(ce => ce.idCamper)
 		const rounds: Round[] = []
 
 		for (const idCamper of idsCampers) {
-			const activitiesPromises = await Promise.all(
+			const activitiesPromises = await Promise.all<Activity[]>(
 				config.activitiesConfig.map(activityConfig => this.activityService.findRandomWhere(activityConfig, idCamper)),
 			)
 			const activities = _.flatMap(activitiesPromises)
-			const round = await Round.create({
-				idCamper,
-				idEdition,
-				blFinished: false,
-				dtBegin: new Date(),
-				dtEnd: null,
-			})
-
-			await Promise.all(activities.map(activity => round.addActivity(activity)))
-			const createdRound = await Round.findByPk(round.idRound, {
-				include: [
-					{
-						model: Activity,
-						foreignKey: 'idActivity',
-						as: 'activities',
-						attributes: ['idActivity', 'dsQuestion', 'tpActivity', 'tpLevel'],
-					} as Includeable,
-				],
-			})
-
+			const createdRound = await this.createRound(idCamper, idEdition, activities)
 			rounds.push(createdRound)
 		}
 
@@ -74,9 +56,11 @@ export class RoundController {
 				} as Includeable,
 			],
 		})
-
+		const { idEdition } = await this.editionService.findCurrent()
 		const activitiesIds = round.activities.map(activity => activity.idActivity)
-		const answers = await CamperActivity.findAll({ where: { idCamper, idActivity: { [Op.in]: activitiesIds } } })
+		const answers = await CamperActivity.findAll({
+			where: { idCamper, idEdition, idActivity: { [Op.in]: activitiesIds } },
+		})
 		const answeredActivities = answers.map(answer => answer.idActivity)
 		const newActivities = round.activities.filter(activity => !answeredActivities.includes(activity.idActivity))
 		delete round.dataValues.activities
@@ -97,5 +81,26 @@ export class RoundController {
 
 		const [rows, data] = await Round.update({ blFinished: true, dtEnd: new Date() }, { where: { idRound } })
 		res.json(data ? data : rows)
+	}
+
+	private async createRound(idCamper: number, idEdition: number, activities: Activity[]) {
+		const round = await Round.create({
+			idCamper,
+			idEdition,
+			blFinished: false,
+			dtBegin: new Date(),
+			dtEnd: null,
+		})
+		await Promise.all(activities.map(activity => round.addActivity(activity)))
+		return Round.findByPk(round.idRound, {
+			include: [
+				{
+					model: Activity,
+					foreignKey: 'idActivity',
+					as: 'activities',
+					attributes: ['idActivity', 'dsQuestion', 'tpActivity', 'tpLevel'],
+				} as Includeable,
+			],
+		})
 	}
 }
--- src/api/services/ActivityService.ts
+++ src/api/services/ActivityService.ts
@@ -3,17 +3,45 @@ import { Activity, CamperActivity } from '../../models'
 import { ActivityConfig } from '../../types/RoundConfig'
 
 export class ActivityService {
-	public async findRandomWhere(config: ActivityConfig, idCamper: number): Promise<Activity[]> {
+	public async findRandomWhere({ nrQuantity, tpLevel, type }: ActivityConfig, idCamper: number): Promise<Activity[]> {
 		const campersActivities = await CamperActivity.findAll({ where: { idCamper } })
 		const activitiesIds = campersActivities.map(ca => ca.idActivity)
-		const { nrQuantity, tpLevel, type } = config
-		return Activity.findAll({
+		const activities = await Activity.findAll({
 			where: {
 				tpActivity: type,
-				tpLevel: tpLevel,
+				tpLevel,
 				idActivity: { [Op.notIn]: activitiesIds },
 			},
 			limit: nrQuantity,
 		})
+
+		if (activities.length < nrQuantity) {
+			return this.loadMissingOnesFromOldestAnswers({ nrQuantity, tpLevel, type }, activities, activitiesIds)
+		}
+
+		return activities
+	}
+
+	private async loadMissingOnesFromOldestAnswers(
+		{ nrQuantity, tpLevel, type }: ActivityConfig,
+		activities: Activity[],
+		activitiesIds: number[],
+	): Promise<Activity[]> {
+		const numberOfMissing = nrQuantity - activities.length
+		const activitiesOldestAnswer = await CamperActivity.findAll({
+			where: { idActivity: { [Op.in]: activitiesIds } },
+			order: [['updatedAt', 'ASC']],
+			attributes: ['idActivity'],
+		})
+		const missing = await Activity.findAll({
+			where: {
+				tpActivity: type,
+				tpLevel,
+				idActivity: { [Op.in]: activitiesOldestAnswer.map(a => a.idActivity) },
+			},
+			limit: numberOfMissing,
+		})
+
+		return [...activities, ...missing]
 	}
 }
