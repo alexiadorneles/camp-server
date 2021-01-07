50cbb6fff954d87de561cc395bd4222e05b8f744
alexiadorneles <lecavsalexia@gmail.com>
2021-01-07 00:08:23 -0300

    fix service.

--- src/api/services/ActivityService.ts
+++ src/api/services/ActivityService.ts
@@ -6,10 +6,16 @@ export class ActivityService {
 	public async findRandomUnanswered(idCamper: number, quantity: number): Promise<Activity[]> {
 		const campersActivities = await CamperActivity.findAll({ where: { idCamper } })
 		const activitiesIds = campersActivities.map(ca => ca.idActivity)
-		return Activity.findAll({
+		const activities = await Activity.findAll({
 			where: { idActivity: { [Op.notIn]: activitiesIds } },
 			limit: quantity,
 		})
+
+		if (activities.length < quantity) {
+			return this.loadMissingOnesFromOldestAnswers(activities, activitiesIds, quantity)
+		}
+
+		return activities
 	}
 
 	public async findRandomWhere({ nrQuantity, tpLevel, type }: ActivityConfig, idCamper: number): Promise<Activity[]> {
@@ -25,13 +31,33 @@ export class ActivityService {
 		})
 
 		if (activities.length < nrQuantity) {
-			return this.loadMissingOnesFromOldestAnswers({ nrQuantity, tpLevel, type }, activities, activitiesIds)
+			return this.loadMissingOnesFromOldestAnswersFromConfig({ nrQuantity, tpLevel, type }, activities, activitiesIds)
 		}
 
 		return activities
 	}
 
 	private async loadMissingOnesFromOldestAnswers(
+		activities: Activity[],
+		activitiesIds: number[],
+		quantity: number,
+	): Promise<Activity[]> {
+		const numberOfMissing = quantity - activities.length
+		const activitiesOldestAnswer = await CamperActivity.findAll({
+			where: { idActivity: { [Op.in]: activitiesIds } },
+			order: [['updatedAt', 'ASC']],
+			attributes: ['idActivity'],
+		})
+
+		const missing = await Activity.findAll({
+			where: { idActivity: { [Op.in]: activitiesOldestAnswer.map(a => a.idActivity) } },
+			limit: numberOfMissing,
+		})
+
+		return [...activities, ...missing]
+	}
+
+	private async loadMissingOnesFromOldestAnswersFromConfig(
 		{ nrQuantity, tpLevel, type }: ActivityConfig,
 		activities: Activity[],
 		activitiesIds: number[],
