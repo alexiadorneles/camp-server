08e6f2682b794c5e4408fc4a180819836755ed7d
alexiadorneles <lecavsalexia@gmail.com>
2021-01-18 23:02:25 -0300

    feat(mythology): map olympians to cabins.

--- src/types/Mythology.ts
+++ src/types/Mythology.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export enum Divinity {
 	ZEUS = 'Zeus',
 	HERA = 'Hera',
@@ -18,5 +20,34 @@ export enum Divinity {
 	NIKE = 'Nike',
 	HEBE = 'Hebe',
 	TYCHE = 'Tique',
-	HECATE = 'Hécate'
+	HECATE = 'Hécate',
 }
+
+export const OLYMPIAN_TO_CABIN_NUMBER: {
+	[key in Divinity]: number
+} = {
+	Zeus: 1,
+	Hera: 2,
+	Poseidon: 3,
+	Demeter: 4,
+	Ares: 5,
+	Atena: 6,
+	Apolo: 7,
+	['Ártemis']: 8,
+	Hefesto: 9,
+	Afrodite: 10,
+	Hermes: 11,
+	Dionísio: 12,
+	Hades: 13,
+	Íris: 14,
+	Hipnos: 15,
+	Nêmesis: 16,
+	Nike: 17,
+	Hebe: 18,
+	Tique: 19,
+	Hécate: 20,
+}
+
+export const CABIN_NUMBER_TO_OLYMPIAN: {
+	[key in number]: Divinity
+} = _.fromPairs(_.toPairs(OLYMPIAN_TO_CABIN_NUMBER).map(([divinity, num]: [Divinity, number]) => [num, divinity]))
