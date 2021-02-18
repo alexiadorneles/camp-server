6b87f106e6bec77eda188c9139b79564b1714175
alexiadorneles <lecavsalexia@gmail.com>
2021-02-17 22:11:41 -0300

    fix(quiron): treat case of camper with no cabin.

--- src/discord/MessageHandler.ts
+++ src/discord/MessageHandler.ts
@@ -5,6 +5,9 @@ import { CABIN_NUMBER_TO_OLYMPIAN, Divinity } from '../types/Mythology'
 const CAMPER_ADDED_IN_CABIN = (id: string, cabin: string) =>
 	`Hey <@${id}>! Acabei de adicionar você no Chalé ${cabin} aqui no Discord :smile:`
 
+const CAMPER_WITH_NO_CABIN = (id: string) =>
+	`<@${id}>, vejo que você é uma pessoa Campista em nosso Acampamento, mas ainda não está em nenhum chalé. Verifique se realizou a inscrição paga/gratuita em acampamento.portalpercyjackson.com`
+
 export class MessageHandler {
 	constructor(private message: Message) {}
 
@@ -13,9 +16,12 @@ export class MessageHandler {
 		return this.message.reply(replyText)
 	}
 
-	public async addCabinRoleToUser(camper: Camper): Promise<void> {
+	public async addCabinRoleToUser(camper: Camper): Promise<void | Message> {
 		const roleName = CABIN_NUMBER_TO_OLYMPIAN[camper.idCabin]
 		const role = this.message.guild.roles.cache.find(role => role.name === roleName)
+		if (!role) {
+			return this.rejectAndReply(CAMPER_WITH_NO_CABIN(camper.dsDiscordID))
+		}
 		this.message.member.roles.add(role)
 		await this.message.react('✅')
 		this.message.channel.send(CAMPER_ADDED_IN_CABIN(camper.dsDiscordID, `de ${role}`))
