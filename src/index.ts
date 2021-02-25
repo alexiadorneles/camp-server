5d4704f791d4ae4e1afeb8a83e3dd6634ed13422
Aléxia <alexiadorneles02@gmail.com>
2021-02-25 09:30:44 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { CamperService } from 'api/services/CamperService'
 import dotenv from 'dotenv'
 import { configureAPI } from './api'
 import { CampBot } from './discord/CampBot'
 import { watchForMissionWinner } from './twitter/TwitterHandler'
 dotenv.config()
 configureAPI()
-new CampBot()
+new CampBot(new CamperService())
 // setInterval(watchForMissionWinner, 15 * 1000)
